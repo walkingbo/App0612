d0ddb556616445574fc66bef4b93ff47dd7fc436
503 <a503_18@503-18ui-iMac.local>
2019-06-12 11:11:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/app0612/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.app0612;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Activity 클래스가 인스턴스 화 된 후 가장 먼저 호출되는 메소드
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //상위 클래스의 메소드를 호출
+        super.onCreate(savedInstanceState);
+        //화면 출력을 위해서 뷰를 설정하는 메소드
+        setContentView(R.layout.activity_main);
+    }
+}
